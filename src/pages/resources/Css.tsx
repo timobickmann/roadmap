3a51531465a1c9da01e4a0150c7b71c05e4b9beb
Timo Bickmann (MB Air) <bickmann.timo@gmail.com>
2023-06-28 16:33:01 +0200

    steps rendered

--- src/pages/resources/Css.tsx
+++ src/pages/resources/Css.tsx
@@ -3,36 +3,53 @@ import SetupStep from "../../components/SetupStep";
 import SetupStepCodeBlock from "../../components/SetupStepCodeBlock";
 
 function Css() {
-  const steps = data.map((element) => {
-    if (element.language === "") {
-      // <SetupStep step={element.step} description={element.description} />;
-      return (
-        <>
-          <div>
-            <h1>Step: {element.step}</h1>
-            <p>Description: {element.description}</p>
-          </div>
-        </>
-      );
-    } else {
-      // <SetupStepCodeBlock
-      //   step={element.step}
-      //   description={element.description}
-      //   language={element.language}
-      //   code={element.code}
-      // />;
-      return (
-        <>
-          <div>
-            <h1>Step: {element.step}</h1>
-            <p>Description: {element.description}</p>
-            <p>Language: {element.language}</p>
-            <p>Code Block: {element.code}</p>
-          </div>
-        </>
-      );
-    }
-  });
-  return <>{steps}</>;
+  return (
+    <>
+      {data.map((element) => {
+        return (
+          <>
+            <div>{element.step}</div>
+            <p>{element.description}</p>
+            <p>{element.language}</p>
+            <p>{element.code}</p>
+          </>
+        );
+      })}
+    </>
+  );
 }
 export default Css;
+
+// function Css() {
+//   return data.map((element) => {
+//     if (element.language === "") {
+//       // <SetupStep step={element.step} description={element.description} />;
+//       return (
+//         <>
+//           <div>
+//             <h1>Step: {element.step}</h1>
+//             <p>Description: {element.description}</p>
+//           </div>
+//         </>
+//       );
+//     } else {
+//       // <SetupStepCodeBlock
+//       //   step={element.step}
+//       //   description={element.description}
+//       //   language={element.language}
+//       //   code={element.code}
+//       // />;
+//       return (
+//         <>
+//           <div>
+//             <h1>Step: {element.step}</h1>
+//             <p>Description: {element.description}</p>
+//             <p>Language: {element.language}</p>
+//             <p>Code Block: {element.code}</p>
+//           </div>
+//         </>
+//       );
+//     }
+//   });
+// }
+// export default Css;
