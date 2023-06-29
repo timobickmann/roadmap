42ba72693bb6c67e0fa03b6e832f07fb502142d2
Timo Bickmann (MB Air) <bickmann.timo@gmail.com>
2023-06-30 00:21:21 +0200

    added first testversion of roadmap

--- src/pages/Roadmap.tsx
+++ src/pages/Roadmap.tsx
@@ -1,11 +1,252 @@
+// @ts-nocheck
+
 function Roadmap() {
   return (
     <>
-      <div className="container mx-auto flex justify-center">
-        <h1 className="text-xl">Roadmap</h1>
-      </div>
-      <img src="roadmap.png" className="mx-auto" />
+      <svg
+        viewBox="0 0 625 1150"
+        xmlns="http://www.w3.org/2000/svg"
+        fillRule="evenodd"
+        clipRule="evenodd"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeMiterlimit={1.5}
+      >
+        <path fill="none" d="M0 0H625V1149.24H0z" />
+        <path
+          d="M311.472 412.969c5.797 27.682 17.151 59.131 17.392 83.044.238 23.751-11.456 59.664-17.392 83.917-5.906 24.129-16.82 58.803-16.926 83.099-.103 23.859 11.284 55.584 16.926 83.376"
+          fill="none"
+          stroke="#fff"
+          strokeWidth="4.17px"
+        />
+        <path
+          d="M311.472 79.636c5.797 27.681 17.151 59.131 17.392 83.044.238 23.751-11.456 59.664-17.392 83.916-5.906 24.13-16.82 58.804-16.926 83.099-.103 23.86 11.284 55.584 16.926 83.376M311.472 746.303c5.797 27.681 17.151 59.131 17.392 83.044.238 23.751-11.456 59.664-17.392 83.916-5.906 24.13-16.82 58.803-16.926 83.099-.103 23.86 11.284 55.584 16.926 83.376"
+          fill="none"
+          stroke="#fff"
+          strokeWidth="4.17px"
+        />
+        <path
+          fill="#fff"
+          stroke="#000"
+          strokeWidth=".83px"
+          d="M270.833 1066.67H354.16600000000005V1091.67H270.833z"
+        />
+        <text
+          x="305.666px"
+          y="1082.51px"
+          fontFamily="'ArialMT','Arial',sans-serif"
+          fontSize="8.989px"
+          transform="matrix(1 0 0 .99928 0 .737)"
+        >
+          {"Life"}
+        </text>
+        <path
+          fill="#fff"
+          stroke="#000"
+          strokeWidth=".83px"
+          d="M254.167 983.333H337.5V1008.333H254.167z"
+        />
+        <text
+          x="279.424px"
+          y="999.173px"
+          fontFamily="'ArialMT','Arial',sans-serif"
+          fontSize="8.989px"
+          transform="matrix(1 0 0 .99928 0 .677)"
+        >
+          {"DevOps"}
+        </text>
+        <path
+          fill="#fff"
+          stroke="#000"
+          strokeWidth=".83px"
+          d="M270.833 900H354.16600000000005V925H270.833z"
+        />
+        <text
+          x="284.593px"
+          y="914.816px"
+          fontFamily="'ArialMT','Arial',sans-serif"
+          fontSize="8.989px"
+          transform="matrix(1 0 0 .99928 0 .616)"
+        >
+          {"Desktop "}
+          <tspan x="319.571px 325.567px" y="914.816px 914.816px">
+            {"Ap"}
+          </tspan>
+          {"ps"}
+        </text>
+        <path
+          fill="#fff"
+          stroke="#000"
+          strokeWidth=".83px"
+          d="M287.5 816.667H370.83299999999997V841.667H287.5z"
+        />
+        <text
+          x="304.521px"
+          y="831.536px"
+          fontFamily="'ArialMT','Arial',sans-serif"
+          fontSize="8.989px"
+          transform="matrix(1 0 0 .99928 0 .555)"
+        >
+          {"Mobile "}
+          <tspan x="333.003px 338.999px" y="831.536px 831.536px">
+            {"Ap"}
+          </tspan>
+          {"ps"}
+        </text>
+        <path
+          fill="#fff"
+          stroke="#000"
+          strokeWidth=".83px"
+          d="M270.833 733.333H354.16600000000005V758.333H270.833z"
+        />
+        <text
+          x="305.666px"
+          y="749.173px"
+          fontFamily="'ArialMT','Arial',sans-serif"
+          fontSize="8.989px"
+          transform="matrix(1 0 0 .99928 0 .496)"
+        >
+          {"API"}
+        </text>
+        <path
+          fill="#fff"
+          stroke="#000"
+          strokeWidth=".83px"
+          d="M254.167 650H337.5V675H254.167z"
+        />
+        <text
+          x="274.172px"
+          y="665.84px"
+          fontFamily="'ArialMT','Arial',sans-serif"
+          fontSize="8.989px"
+          transform="matrix(1 0 0 .99928 0 .435)"
+        >
+          {"Databases"}
+        </text>
+        <path
+          fill="#fff"
+          stroke="#000"
+          strokeWidth=".83px"
+          d="M270.833 566.667H354.16600000000005V591.667H270.833z"
+        />
+        <text
+          x="298.34px"
+          y="581.483px"
+          fontFamily="'ArialMT','Arial',sans-serif"
+          fontSize="8.989px"
+          transform="matrix(1 0 0 .99928 0 .374)"
+        >
+          {"Nodejs"}
+        </text>
+        <g>
+          <path
+            fill="#fff"
+            stroke="#000"
+            strokeWidth=".83px"
+            d="M287.5 483.333H370.83299999999997V508.333H287.5z"
+          />
+          <text
+            x="319.78px"
+            y="499.173px"
+            fontFamily="'ArialMT','Arial',sans-serif"
+            fontSize="8.989px"
+            transform="matrix(1 0 0 .99928 0 .315)"
+          >
+            {"SSR"}
+          </text>
+        </g>
+        <g>
+          <path
+            fill="#fff"
+            stroke="#000"
+            strokeWidth=".83px"
+            d="M270.833 400H354.16600000000005V425H270.833z"
+          />
+          <text
+            x="296.731px"
+            y="415.84px"
+            fontFamily="'ArialMT','Arial',sans-serif"
+            fontSize="8.989px"
+            transform="matrix(1 0 0 .99928 0 .254)"
+          >
+            {"T"}
+            <tspan x="301.226px 306.225px" y="415.84px 415.84px">
+              {"ai"}
+            </tspan>
+            {"lwind"}
+          </text>
+        </g>
+        <g>
+          <path
+            fill="#fff"
+            stroke="#000"
+            strokeWidth=".83px"
+            d="M254.167 316.667H337.5V341.667H254.167z"
+          />
+          <text
+            x="283.398px"
+            y="332.507px"
+            fontFamily="'ArialMT','Arial',sans-serif"
+            fontSize="8.989px"
+            transform="matrix(1 0 0 .99928 0 .194)"
+          >
+            {"React"}
+          </text>
+        </g>
+        <g>
+          <path
+            fill="#fff"
+            stroke="#000"
+            strokeWidth=".83px"
+            d="M270.833 233.333H354.16600000000005V258.33299999999997H270.833z"
+          />
+          <text
+            x="307.36px"
+            y="249.068px"
+            fontFamily="'ArialMT','Arial',sans-serif"
+            fontSize="8.989px"
+            transform="matrix(1 0 0 .99928 0 .134)"
+          >
+            {"JS"}
+          </text>
+        </g>
+        <g>
+          <path
+            fill="#fff"
+            stroke="#000"
+            strokeWidth=".83px"
+            d="M287.5 150H370.83299999999997V175H287.5z"
+          />
+          <text
+            x="319.936px"
+            y="165.797px"
+            fontFamily="'ArialMT','Arial',sans-serif"
+            fontSize="8.989px"
+            transform="matrix(1 0 0 .99928 0 .073)"
+          >
+            {"CSS"}
+          </text>
+        </g>
+        <g>
+          <path
+            fill="#fff"
+            stroke="#000"
+            strokeWidth=".83px"
+            d="M270.833 66.667H354.16600000000005V91.667H270.833z"
+          />
+          <text
+            x="300.065px"
+            y="82.507px"
+            fontFamily="'ArialMT','Arial',sans-serif"
+            fontSize="8.989px"
+            transform="matrix(1 0 0 .99928 0 .013)"
+          >
+            {"HTML"}
+          </text>
+        </g>
+      </svg>
     </>
   );
 }
+
 export default Roadmap;
