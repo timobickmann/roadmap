6522d25437a018c5e229785abc5a11d64a71d4cf
Timo Bickmann (MB Air) <bickmann.timo@gmail.com>
2023-06-29 10:34:28 +0200

    added codeblock component

---
+++ src/components/CodeBlock.tsx
@@ -0,0 +1,35 @@
+import { FaCopy } from "react-icons/fa6";
+
+interface IProps {
+  language: string;
+  code: string;
+  file: string;
+}
+
+function CodeBlock({ language, code, file }: IProps) {
+  return (
+    <>
+      <pre>
+        <div className="mt-4 w-min rounded-md bg-black pb-1 font-sans">
+          <div className="flex items-center justify-between rounded-t-md bg-gray-800 px-4 py-2 text-xs">
+            <span>{language}</span>
+            <span className="px-10">{file}</span>
+            <button
+              onClick={() => {
+                navigator.clipboard.writeText(code);
+              }}
+              className="flex items-center gap-2"
+            >
+              <FaCopy />
+              copy code
+            </button>
+          </div>
+          <div className="m-5 overflow-y-auto text-sm">
+            <code className="">{code}</code>
+          </div>
+        </div>
+      </pre>
+    </>
+  );
+}
+export default CodeBlock;
--- src/components/SetupStep.tsx
+++ src/components/SetupStep.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { FaCopy } from "react-icons/fa6";
+import CodeBlock from "./CodeBlock";
 
 interface IProps {
   step: string;
@@ -24,35 +24,16 @@ function SetupStep({
     <>
       {language === "" ? (
         <div className="container mx-auto my-10">
-          <h1>Step: {step}</h1>
+          <h1>{step}</h1>
           <p>{description}</p>
           <Link to={link}>{linktext}</Link>
         </div>
       ) : (
         <div className="container mx-auto my-10">
-          <h1>Step: {step}</h1>
+          <h1>{step}</h1>
           <p>{description}</p>
           <Link to={link}>{linktext}</Link>
-          <pre>
-            <div className="w-min rounded-md bg-black pb-1 font-sans mt-4">
-              <div className="flex items-center justify-between rounded-t-md bg-gray-800 px-4 py-2 text-xs">
-                <span>{language}</span>
-                <span className="px-10">{file}</span>
-                <button
-                  onClick={() => {
-                    navigator.clipboard.writeText(code);
-                  }}
-                  className="flex items-center gap-2"
-                >
-                  <FaCopy />
-                  copy code
-                </button>
-              </div>
-              <div className="m-5 overflow-y-auto text-sm">
-                <code className="">{code}</code>
-              </div>
-            </div>
-          </pre>
+          <CodeBlock file={file} language={language} code={code} />
         </div>
       )}
     </>
