ab40e6a75bd38ecaca91e0b4725ba487ff585413
Timo Bickmann (MB Air) <bickmann.timo@gmail.com>
2023-07-04 13:57:44 +0200

    built toggle for sidebar items

--- src/components/SidebarResources.tsx
+++ src/components/SidebarResources.tsx
@@ -3,7 +3,7 @@ import SidebarResourcesItem from "./SidebarResourcesItem";
 function SidebarResources() {
   return (
     <>
-      <aside className="fixed bottom-0 left-0 top-0 z-20 min-h-screen overflow-y-scroll bg-gray-800 px-6 scrollbar-hide">
+      <aside className="fixed bottom-0 left-0 top-0 z-20 min-h-screen w-40 overflow-y-scroll bg-gray-800 px-6 scrollbar-hide">
         <nav className="">
           <ul className="flex flex-col gap-5 py-10">
             <SidebarResourcesItem item="Vite" />
--- src/components/SidebarResourcesItem.tsx
+++ src/components/SidebarResourcesItem.tsx
@@ -1,25 +1,39 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { FaCaretDown } from "react-icons/fa6";
+import { FaCaretUp } from "react-icons/fa6";
 
 interface IProps {
   item: string;
 }
 
 function SidebarResourcesItem({ item }: IProps) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handleClick() {
+    setIsOpen(!isOpen);
+  }
+
   return (
     <>
       <li>
-        {item}
-        <ul>
-          <li>
-            <Link to={`/resources/${item.toLowerCase()}/setup`}>Setup</Link>
-          </li>
-          <li>
-            <Link to={`/resources/${item.toLowerCase()}`}>Resources</Link>
-          </li>
-          <li>
-            <Link to={`/resources/${item.toLowerCase()}/howtos`}>How-Tos</Link>
-          </li>
-        </ul>
+        <button className="flex items-center gap-2" onClick={handleClick}>{item} {isOpen ? <FaCaretUp/> : <FaCaretDown/>}</button>
+
+        {isOpen && (
+          <ul>
+            <li>
+              <Link to={`/resources/${item.toLowerCase()}/setup`}>Setup</Link>
+            </li>
+            <li>
+              <Link to={`/resources/${item.toLowerCase()}`}>Resources</Link>
+            </li>
+            <li>
+              <Link to={`/resources/${item.toLowerCase()}/howtos`}>
+                How-Tos
+              </Link>
+            </li>
+          </ul>
+        )}
       </li>
     </>
   );
