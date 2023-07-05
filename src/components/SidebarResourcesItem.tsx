1d2d3c6ed810a1ff21a026edb5b53dc5b3ecb440
Timo Bickmann (MB Air) <bickmann.timo@gmail.com>
2023-07-05 17:49:33 +0200

    moved state from sidebar to appcontext

--- src/components/SidebarResourcesItem.tsx
+++ src/components/SidebarResourcesItem.tsx
@@ -1,13 +1,18 @@
+//@ts-nocheck
+
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useState, useContext } from "react";
 import { FaCaretDown } from "react-icons/fa6";
 import { FaCaretUp } from "react-icons/fa6";
+import { AppContext } from "../AppContext";
 
 interface IProps {
   item: string;
 }
 
 function SidebarResourcesItem({ item }: IProps) {
+  const { sidebarIsOpened, toggleSidebarIsOpened } = useContext(AppContext);
+
   const [isOpen, setIsOpen] = useState(false);
 
   function handleClick() {
@@ -17,18 +22,33 @@ function SidebarResourcesItem({ item }: IProps) {
   return (
     <>
       <li>
-        <button className="flex items-center gap-2" onClick={handleClick}>{item} {isOpen ? <FaCaretUp/> : <FaCaretDown/>}</button>
+        <button className="flex items-center gap-2" onClick={handleClick}>
+          {item} {isOpen ? <FaCaretUp /> : <FaCaretDown />}
+        </button>
 
         {isOpen && (
           <ul>
             <li>
-              <Link to={`/resources/${item.toLowerCase()}/setup`}>Setup</Link>
+              <Link
+                onClick={toggleSidebarIsOpened}
+                to={`/resources/${item.toLowerCase()}/setup`}
+              >
+                Setup
+              </Link>
             </li>
             <li>
-              <Link to={`/resources/${item.toLowerCase()}`}>Resources</Link>
+              <Link
+                onClick={toggleSidebarIsOpened}
+                to={`/resources/${item.toLowerCase()}`}
+              >
+                Resources
+              </Link>
             </li>
             <li>
-              <Link to={`/resources/${item.toLowerCase()}/howtos`}>
+              <Link
+                onClick={toggleSidebarIsOpened}
+                to={`/resources/${item.toLowerCase()}/howtos`}
+              >
                 How-Tos
               </Link>
             </li>
