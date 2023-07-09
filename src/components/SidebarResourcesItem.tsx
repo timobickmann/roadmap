a7606a643403f967c79c46c7119e1504588b9861
Timo Bickmann (MB Air) <bickmann.timo@gmail.com>
2023-07-09 23:55:42 +0200

    only render sidebar item when there is a site

--- src/components/SidebarResourcesItem.tsx
+++ src/components/SidebarResourcesItem.tsx
@@ -1,6 +1,9 @@
 import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { AppContext } from "../AppContext";
+import setupData from "../data/setup.json";
+import resourcesData from "../data/resources.json";
+import howTosData from "../data/howTos.json";
 
 interface IProps {
   sidebarItem: string;
@@ -8,32 +11,43 @@ interface IProps {
 
 function SidebarResourcesItem({ sidebarItem }: IProps) {
   const { toggleSidebarIsOpened } = useContext(AppContext);
+  const isSetup = setupData.find((item) => item.category === sidebarItem);
+  const isResource = resourcesData.find(
+    (item) => item.category === sidebarItem
+  );
+  const isHowTo = howTosData.find((item) => item.category === sidebarItem);
 
   return (
     <>
-      <Link
-        className="rounded px-4 py-1 hover:bg-neutral hover:text-neutral-content"
-        onClick={toggleSidebarIsOpened}
-        to={`/resources/${sidebarItem}/setup`}
-      >
-        Setup
-      </Link>
+      {isSetup && (
+        <Link
+          className="rounded px-4 py-1 hover:bg-neutral hover:text-neutral-content"
+          onClick={toggleSidebarIsOpened}
+          to={`/resources/${sidebarItem}/setup`}
+        >
+          Setup
+        </Link>
+      )}
 
-      <Link
-        className="rounded px-4 py-1 hover:bg-neutral hover:text-neutral-content"
-        onClick={toggleSidebarIsOpened}
-        to={`/resources/${sidebarItem}`}
-      >
-        Resources
-      </Link>
+      {isResource && (
+        <Link
+          className="rounded px-4 py-1 hover:bg-neutral hover:text-neutral-content"
+          onClick={toggleSidebarIsOpened}
+          to={`/resources/${sidebarItem}`}
+        >
+          Resources
+        </Link>
+      )}
 
-      <Link
-        className="rounded px-4 py-1 hover:bg-neutral hover:text-neutral-content"
-        onClick={toggleSidebarIsOpened}
-        to={`/resources/${sidebarItem}/howtos`}
-      >
-        How-Tos
-      </Link>
+      {isHowTo && (
+        <Link
+          className="rounded px-4 py-1 hover:bg-neutral hover:text-neutral-content"
+          onClick={toggleSidebarIsOpened}
+          to={`/resources/${sidebarItem}/howtos`}
+        >
+          How-Tos
+        </Link>
+      )}
     </>
   );
 }
