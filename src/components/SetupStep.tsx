92bc940443c6e7b577f574d1123387e6e2aedd4c
Timo Bickmann (MB Air) <bickmann.timo@gmail.com>
2023-07-09 21:14:48 +0200

    added md data support and md styling

--- src/components/SetupStep.tsx
+++ src/components/SetupStep.tsx
@@ -1,44 +1,22 @@
-import { Link } from "react-router-dom";
-import CodeBlock from "./CodeBlock";
-import data from "../data/setup.json";
+import setupData from "../data/setup.json";
+import { ReactMarkdown } from "react-markdown/lib/react-markdown";
+import style from "./markdownStyles.module.css"
 
 interface IProps {
   category: string;
 }
 
 function SetupStep({ category }: IProps) {
-  const findData = data.filter((item) => item.category === category);
+  const setupMarkdown = setupData.filter((item) => item.category === category);
   return (
     <>
-      {findData.map((element) => {
+      {setupMarkdown.map((element) => {
         return (
           <>
-            <div className="mb-5 flex flex-col gap-1 text-center sm:text-left">
-              <h2 className="mb-3 text-center text-2xl sm:text-left">
-                {element.step}
-              </h2>
-              {element.description !== "" && (
-                <p className="whitespace-pre-wrap">{element.description}</p>
-              )}
-              {element.link !== "" && (
-                <Link
-                  to={element.link}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  {element.linktext}
-                </Link>
-              )}
-              {element.code !== "" && (
-                <div className="mx-auto sm:mx-0">
-                  <CodeBlock
-                    file={element.file}
-                    language={element.language}
-                    code={element.code}
-                  />
-                </div>
-              )}
-            </div>
+              {element.md && <ReactMarkdown
+             className={style.reactMarkdown} 
+              children={element.md}/>}
+              
           </>
         );
       })}
