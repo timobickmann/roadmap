e08dc70f20e604f3455d3cf6d648a5f686fd9e70
Timo Bickmann (MB Air) <bickmann.timo@gmail.com>
2023-07-09 22:42:24 +0200

    added code highlighting

--- src/components/SetupStep.tsx
+++ src/components/SetupStep.tsx
@@ -1,3 +1,4 @@
+//@ts-nocheck
 import setupData from "../data/setup.json";
 import { ReactMarkdown } from "react-markdown/lib/react-markdown";
 import style from "./markdownStyles.module.css";
@@ -16,12 +17,29 @@ function SetupStep({ category }: IProps) {
       {setupMarkdown.map((element) => {
         return (
           <>
-            {element.md && (
+            {element.md && (<>
               <ReactMarkdown
                 linkTarget={"_blank"}
                 className={style.reactMarkdown}
                 children={element.md}
-              />
+                components={{
+                  code({node, inline, className, children, ...props}) {
+                    const match = /language-(\w+)/.exec(className || '')
+                    return !inline && match ? (
+                      <SyntaxHighlighter
+                        {...props}
+                        children={String(children).replace(/\n$/, '')}
+                        style={dark}
+                        language={match[1]}
+                      />
+                    ) : (
+                      <code {...props} className={className}>
+                        {children}
+                      </code>
+                    )
+                  }
+                }}
+              /></>
             )}
           </>
         );
