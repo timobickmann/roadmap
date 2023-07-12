a6ebde51767c122bd0a399064eb35092f6f391ef
Timo Bickmann (MB Air) <bickmann.timo@gmail.com>
2023-07-12 20:27:05 +0200

    installed nodemon and setup dev script

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
-const express = require("express")
+const express = require("express");
 
-const app = express()
+const app = express();
 
+app.listen(4000, () => console.log("listening on port 4000"));
