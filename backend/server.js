f29d80d01ffd75da003255bfd8442e9c9ad945e2
Timo Bickmann (MB Air) <bickmann.timo@gmail.com>
2023-07-13 01:37:17 +0200

    cors added

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,11 @@ const express = require("express");
 const roadmapStatusRoutes = require("./routes/roadmapStatus");
 const mongoose = require("mongoose");
 require("dotenv").config();
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
