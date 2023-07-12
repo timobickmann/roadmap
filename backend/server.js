3e646f686df19089ee6075ddb680af046c8621af
Timo Bickmann (MB Air) <bickmann.timo@gmail.com>
2023-07-12 20:38:04 +0200

    installed dotenv

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,10 @@
 const express = require("express");
+require("dotenv").config()
 
 const app = express();
 
-app.listen(4000, () => console.log("listening on port 4000"));
+app.get("/", (req, res) => {
+  res.json({ mssg: "Welcome to the app" });
+});
+
+app.listen(process.env.PORT, () => console.log("listening on port", process.env.PORT));
