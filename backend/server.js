931afa39773a53b20d951c91ced60f000a3de2fc
Timo Bickmann (MB Air) <bickmann.timo@gmail.com>
2023-07-18 15:05:31 +0200

    added user model and controller, signup and login

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const roadmapStatusRoutes = require("./routes/roadmapStatus");
+const userRoutes = require("./routes/user");
 const mongoose = require("mongoose");
 require("dotenv").config();
 const cors = require("cors");
@@ -16,6 +17,7 @@ app.use((req, res, next) => {
 });
 
 app.use("/api/roadmap-status", roadmapStatusRoutes);
+app.use("/api/user", userRoutes);
 
 mongoose
   .connect(process.env.MONGO_URI)
